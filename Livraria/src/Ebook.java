beea55a61542971e9267664beb1c2bae5a7ab762
Adriano Gomes <112133600+Adrianodvs013@users.noreply.github.com>
2023-06-05 19:47:42 -0300

    criando a classe ebook

--- Livraria/src/Ebook.java
+++ Livraria/src/Ebook.java
@@ -1,3 +1,66 @@
-public class Ebook {
+public class Ebook extends Livro {
+
+             private String nome;
+             private String descricao;
+             private double valor;
+             private String isbn;
+             private Autor autor;
+             private String waterMark;
+
+             public Ebook(Autor autor) {
+                          super(autor);
+             }
+
+             public void setWaterMark(String waterMark) {
+                          this.waterMark = waterMark;
+             }
+
+             public String getWaterMark() {
+                          return waterMark;
+             }
+
+             public double getValor() {
+                          return valor;
+             }
+
+             public void setValor(double valor) {
+                          this.valor = valor;
+             }
+
+             boolean temAutor() {
+                          return this.autor != null;
+             }
+
+             public String getNome() {
+                          return nome;
+             }
+
+             public void setNome(String nome) {
+                          this.nome = nome;
+             }
+
+             public String getDescricao() {
+                          return descricao;
+             }
+
+             public void setDescricao(String descricao) {
+                          this.descricao = descricao;
+             }
+
+             public String getIsbn() {
+                          return isbn;
+             }
+
+             public void setIsbn(String isbn) {
+                          this.isbn = isbn;
+             }
+
+             public Autor getAutor() {
+                          return autor;
+             }
+
+             public void setAutor(Autor autor) {
+                          this.autor = autor;
+             }
 
 }
