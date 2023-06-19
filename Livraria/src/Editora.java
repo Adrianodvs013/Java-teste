a5c3a6d117116ad331bbdb67ba0393f8526758e0
Adriano Gomes <112133600+Adrianodvs013@users.noreply.github.com>
2023-06-18 22:01:50 -0300

    cap. 7

---
+++ Livraria/src/Editora.java
@@ -0,0 +1,34 @@
+
+public class Editora {
+
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String cnpj;
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nome) {
+        this.nomeFantasia = nome;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    
+
+}
